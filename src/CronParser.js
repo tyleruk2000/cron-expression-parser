d7aba856cd66701a8270cb950445297f176de01d
Tyler Allen <tallen@dalet.com>
2020-02-14 13:36:53 +0000

    Add in check for no args

--- src/CronParser.js
+++ src/CronParser.js
@@ -18,6 +18,12 @@ function parseExpersion(cronArray){
 
 async function testExpersion(cronExpersion){
 
+    if (cronExpersion.length == 0){
+        console.log(chalk.red.bold("No expersion found!"));
+        console.log("usage: npm start [cron expresion] [command]")
+        process.exit()
+    }
+
     for (let index = 0; index < cronExpersion.length; index++) {
 
         const expersion = cronExpersion[index]
@@ -27,11 +33,14 @@ async function testExpersion(cronExpersion){
             console.log(chalk.red.bold("Invalid Cron Expersion: " + expersion));
             process.exit()
         }
-      }
+    }
     
 }
 
+//*/15 0 1,15 * 1-5`
 function parseMinute(cronExpression){
+
+    console.log("Minute: " + cronExpression)
 
     return ""
 }
